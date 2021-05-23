f6279d8f618b7d1f0ef1b2a5cc653eb06d1bd873
natalie-coder <63603124+natalie-coder@users.noreply.github.com>
2021-05-23 21:36:24 +0100

    create file

---
+++ 03-Javascript-Fundamentals/javascript-practice/javascript.js
@@ -0,0 +1,37 @@
+console.log("Hello part-timers");
+//if a name is a coaches' name , log "its a coach"
+const coachesName = "andy";
+if (coachesName == "andy") {
+    console.log("its a coach");
+}
+//if a greeting is bonjour, log "comme ca va"
+const greeting = "bonjour";
+if (greeting == "bonjour") {
+    console.log("comme ca va");
+}
+// if a score reaches 10, log "you won the game"
+const score = 10;
+if (score == 10) {
+    console.log("you won the game");
+}
+//if age is greater than 65, log "time to retire"
+const age = 66;
+if (age >= 65) {
+    console.log ("time to retire");
+}
+//if hasCovid log "time to isolate" else "keep social distancing"
+const hasCovid = true;
+if (hasCovid == true) {
+    console.log("time to isolate");
+} else {
+    console.log("keep social distancing");
+}
+//if age is greater than 18 and isBritish, log "you can vote in the next election"
+const voterAge = 19
+const isBritish = true;
+if ((voterAge > 18) && (isBritish == true)) {
+    console.log("you can vote in the next election");
+} else {
+    console.log("you cannot vote in the next election");
+}
+
